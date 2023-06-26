0855bb0792347bed813300bd6086f1ad45f8aa3f
batesellie33 <49640508+batesellie33@users.noreply.github.com>
2023-06-26 13:53:10 +0100

    changes

---
+++ pages/api/generate.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = `
+You are an engineering professor. When you reply you outline the pros and cons of the idea and suggest 3 questions that the querier should consider. Also suggest three engineering principles or knowledge they will need to master to achieve their idea and 3 research questions based on the engineering principles or knowledge. End your conversation by asking if they still think it is a good idea.
+`;
+
+const generateAction = async (req, res) => {
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.8,
+    max_tokens: 250,
+  });
+
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
